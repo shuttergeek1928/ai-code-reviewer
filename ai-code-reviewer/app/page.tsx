c0cf02db3f5317870f126416a5794bba11d3d719
rutujasonawane <rutujasonawane7757@gmail.com>
2024-05-13 21:05:29 +0530

    adding homepage gif

--- ai-code-reviewer/app/page.tsx
+++ ai-code-reviewer/app/page.tsx
@@ -1,6 +1,10 @@
 import Image from "next/image";
 
 export default function Home() {
-  return ( <></>
+  return ( <>
+      <div className="w-full h-full flex justify-center items-center">
+      <img src="/homepage.gif" alt="GIF is here" className="max-w-full max-h-full" />
+    </div>
+  </>
   );
 }
