e7c6d090268c8b617f16c968f93c1871259c61f6
rutujasonawane <rutujasonawane7757@gmail.com>
2024-05-22 14:28:28 +0530

    Adding code comparison

--- ai-code-reviewer/app/history/page.tsx
+++ ai-code-reviewer/app/history/page.tsx
@@ -4,16 +4,33 @@ import React from "react";
 const History = () => {
   return (
     <>
+      {/* <div className="flex flex-wrap justify-center gap-2">
+        <HistoryCard />
+        <HistoryCard />
+        <HistoryCard />
+        <HistoryCard />
+        <HistoryCard />
+        <HistoryCard />
+        <HistoryCard />
+        <HistoryCard />
+        <HistoryCard />
+      </div> */}
       <div className="flex flex-wrap justify-center w-full h-full overflow-y-scroll gap-16">
-        <HistoryCard />
-        <HistoryCard />
-        <HistoryCard />
-        <HistoryCard />
-        <HistoryCard />
-        <HistoryCard />
-        <HistoryCard />
-        <HistoryCard />
-        <HistoryCard />
+        <div className="flex justify-center w-full">
+          <HistoryCard />
+          <HistoryCard />
+          <HistoryCard />
+        </div>
+        <div className="flex justify-center w-full">
+          <HistoryCard />
+          <HistoryCard />
+          <HistoryCard />
+        </div>
+        <div className="flex justify-center w-full">
+          <HistoryCard />
+          <HistoryCard />
+          <HistoryCard />
+        </div>
       </div>
     </>
   );
