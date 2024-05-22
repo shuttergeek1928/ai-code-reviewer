007b4f174ea1f6f67524387bb55edca11491376d
atishay-vishwakarma <atishay849@gmail.com>
2024-05-23 01:04:30 +0530

    Charting and History page fixes

--- ai-code-reviewer/app/history/page.tsx
+++ ai-code-reviewer/app/history/page.tsx
@@ -1,36 +1,47 @@
+"use client";
 import HistoryCard from "@/components/historycard";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { HistoryResponse, TrendsResponse } from "../models/responseModel";
 
+let historyArrayList: HistoryResponse[] = [];
 const History = () => {
+  const [historyList, setHistoryList] = useState<HistoryResponse[] | null>(
+    null
+  );
+
+  useEffect(() => {
+    const axios = require("axios");
+    let config = {
+      method: "get",
+      maxBodyLength: Infinity,
+      url: "http://localhost:5000/getreview/rutuja@gmail.com",
+      headers: {},
+    };
+    axios
+      .request(config)
+      .then((response: { data: any }) => {
+        setHistoryList(response.data);
+        //console.log(response.data);
+        //console.log(historyList);
+        historyArrayList = response.data;
+        console.log(historyArrayList);
+      })
+      .catch((error: any) => {
+        console.log(error);
+      })
+      .finally();
+  }, []);
+
   return (
     <>
-      {/* <div className="flex flex-wrap justify-center gap-2">
-        <HistoryCard />
-        <HistoryCard />
-        <HistoryCard />
-        <HistoryCard />
-        <HistoryCard />
-        <HistoryCard />
-        <HistoryCard />
-        <HistoryCard />
-        <HistoryCard />
-      </div> */}
-      <div className="flex flex-wrap justify-center w-full h-full overflow-y-scroll gap-16">
-        <div className="flex justify-center w-full">
-          <HistoryCard />
-          <HistoryCard />
-          <HistoryCard />
-        </div>
-        <div className="flex justify-center w-full">
-          <HistoryCard />
-          <HistoryCard />
-          <HistoryCard />
-        </div>
-        <div className="flex justify-center w-full">
-          <HistoryCard />
-          <HistoryCard />
-          <HistoryCard />
-        </div>
+      <div className="flex flex-row flex-wrap justify-center items-center w-full overflow-y-scroll gap-8">
+        {historyArrayList.map((history) => (
+          <HistoryCard
+            inputCode={history.input}
+            outputCode={history.output}
+            reviewId={history.review_id}
+          />
+        ))}
       </div>
     </>
   );
--- ai-code-reviewer/components/barchart.tsx
+++ ai-code-reviewer/components/barchart.tsx
@@ -1,9 +1,9 @@
-'use client'
-import React, { PureComponent } from 'react';
+"use client";
+import { TrendsResponse } from "@/app/models/responseModel";
+import React, { PureComponent, useEffect, useState } from "react";
 import {
   ComposedChart,
   Line,
-  Area,
   Bar,
   XAxis,
   YAxis,
@@ -11,57 +11,82 @@ import {
   Tooltip,
   Legend,
   ResponsiveContainer,
-} from 'recharts';
+} from "recharts";
 
 const data = [
   {
-    name: 'Page A',
+    name: "Page A",
     uv: 590,
     pv: 800,
     amt: 1400,
   },
   {
-    name: 'Page B',
+    name: "Page B",
     uv: 868,
     pv: 967,
     amt: 1506,
   },
   {
-    name: 'Page C',
+    name: "Page C",
     uv: 1397,
     pv: 1098,
     amt: 989,
   },
   {
-    name: 'Page D',
+    name: "Page D",
     uv: 1480,
     pv: 1200,
     amt: 1228,
   },
   {
-    name: 'Page E',
+    name: "Page E",
     uv: 1520,
     pv: 1108,
     amt: 1100,
   },
   {
-    name: 'Page F',
+    name: "Page F",
     uv: 1400,
     pv: 680,
     amt: 1700,
   },
 ];
 
-export default class BarCharts extends PureComponent {
-  static demoUrl = 'https://codesandbox.io/p/sandbox/composed-chart-of-same-data-3cs8ym';
+const Code = () => {};
 
-  render() {
-    return (
+const BarCharts = () => {
+  const [trends, setTrends] = useState<TrendsResponse[]>();
+
+  useEffect(() => {
+    const axios = require("axios");
+    let config = {
+      method: "post",
+      maxBodyLength: Infinity,
+      url: "http://localhost:5000/trends",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      data: { email: "rutuja@gmail.com" },
+    };
+    axios
+      .request(config)
+      .then((response: { data: any }) => {
+        setTrends(response.data);
+        console.log(trends);
+      })
+      .catch((error: any) => {
+        console.log(error);
+      })
+      .finally(function () {});
+  }, []);
+
+  return (
+    <>
       <ResponsiveContainer width="100%" height="100%">
         <ComposedChart
           width={500}
           height={400}
-          data={data}
+          data={trends}
           margin={{
             top: 20,
             right: 20,
@@ -70,14 +95,16 @@ export default class BarCharts extends PureComponent {
           }}
         >
           <CartesianGrid stroke="#f5f5f5" />
-          <XAxis dataKey="name" scale="band" />
+          <XAxis dataKey="review_id" scale="band" />
           <YAxis />
           <Tooltip />
           <Legend />
-          <Bar dataKey="uv" barSize={20} fill="#413ea0" />
-          <Line type="monotone" dataKey="uv" stroke="#ff7300" />
+          <Bar dataKey="accuracy" barSize={80} fill="#413ea0" />
+          <Line type="monotone" dataKey="accuracy" stroke="#ff7300" />
         </ComposedChart>
       </ResponsiveContainer>
-    );
-  }
-}
+    </>
+  );
+};
+
+export default BarCharts;
